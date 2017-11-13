04738040293cbb0bdb25e04a81e743151692c0d0
Ali Khan <alik@robarts.ca>
2017-11-13 10:53:39 -0500

    updates for v0.0.2

--- install_scripts/00.install_basics_sudo.sh
+++ install_scripts/00.install_basics_sudo.sh
@@ -24,13 +24,13 @@ apt-get install -y build-essential libtool autotools-dev automake autoconf
 apt-get install -y libglu1-mesa libsm6 libxt-dev libglib2.0-dev libqt5x11extras5
 
 #needed when install dcm4che
-apt-get install -y default-jre
+#apt-get install -y default-jre
 
 #needed when run freesurfer
 apt-get install -y libqt4-scripttools libqt4-dev libjpeg62
 
 #needed when run elastix
-apt-get install -y ocl-icd-opencl-dev
+#apt-get install -y ocl-icd-opencl-dev
 
 #need when install minc
 apt-get install -y imagemagick
--- install_scripts/26.install_vasst_dev_by_source.sh
+++ install_scripts/26.install_vasst_dev_by_source.sh
@@ -30,6 +30,10 @@ rm -rf $TMP_DIR
 
 
 git clone https://github.com/akhanf/vasst-dev $INSTALL/vasst-dev
+pushd $INSTALL/vasst-dev
+git checkout v0.0.1
+popd
+
 
 #if octave exists:
 if [ -e /etc/octave.conf ]
